a576d3e40bd6a916b391234d23379a4b1f07fdba
zoirovdev <abboszoirov17@gmail.com>
2025-04-22 22:47:40 +0500

    add all

--- main.js
+++ main.js
@@ -15,36 +15,62 @@ searchEl.addEventListener("keyup", (event) => {
 
 getAll()
 
-function getAll(){
-	const cardEl = document.createElement("div");
-	cardEl.classList.add("card");
+async function getAll(){
+	const ACCESS_KEY = 'rLctyv9Gt88pPG_AFlfnPWK_AMQnDuPoCES641c85dA'
 
-	const anchorEl = document.createElement("a");
-	anchorEl.href = "detail.html?image=fox.jpg";
+	const response = await fetch('https://api.unsplash.com/photos?page=1&per_page=10', {
+		headers : {
+			Authorization: `Client-ID ${ACCESS_KEY}`
+		}
+	})
 
-	const imgEl = document.createElement("img");
-	imgEl.src = 'fox.jpg';
-	imgEl.alt = 'fox'
-
-	anchorEl.appendChild(imgEl);
-
-	const btnContainer = document.createElement("div");
-	btnContainer.classList.add("btn-container");
-
-	const btnEl = document.createElement("button");
-	btnEl.type = "button"
-	btnEl.classList.add("btn");
-	btnEl.id = "download"
+	const data = await response.json()
 
 
-	btnContainer.appendChild(btnEl);
+	for(let i = 0; i < 10; i++){
+		
+		console.log(data[i]);
+		const cardEl = document.createElement("div");
+		cardEl.classList.add("card");
 
-	const iconEl = document.createElement("i");
-	iconEl.classList.add("fas", "fa-arrow-down")
+		const anchorEl = document.createElement("a");
+		const params = new URLSearchParams({ image: `${data[i].urls.small}`, id: data[i].id, slug: data[i].slug,
+			created_at: data[i].created_at, description: data[i].alt_description, updated_at: data[i].updated_at,
+			height: data[i].height, width: data[i].width });
 
-	btnEl.appendChild(iconEl);
+		anchorEl.href = `detail.html?${params.toString()}`;
+
+		const imgEl = document.createElement("img");
+		imgEl.src = `${data[i].urls.small}`
+		imgEl.alt = 'fox'
+
+		anchorEl.appendChild(imgEl);
+
+		const btnContainer = document.createElement("div");
+		btnContainer.classList.add("btn-container");
+
+		const btnEl = document.createElement("button");
+		btnEl.type = "button"
+		btnEl.classList.add("btn");
+		btnEl.id = "download"
+
+		btnEl.addEventListener("click", () => {
+			const link = document.createElement("a");
+			link.href = `${data[i].links.download}`;
+			link.download = `${data[i].urls.small}`;
+			link.click();
+		});
+
+
+		btnContainer.appendChild(btnEl);
+
+		const iconEl = document.createElement("i");
+		iconEl.classList.add("fas", "fa-arrow-down")
+
+		btnEl.appendChild(iconEl);
 	
-	cardEl.appendChild(anchorEl);
-	cardEl.appendChild(btnContainer);
-	containerEl.appendChild(cardEl);
+		cardEl.appendChild(anchorEl);
+		cardEl.appendChild(btnContainer);
+		containerEl.appendChild(cardEl);
+	}
 }
