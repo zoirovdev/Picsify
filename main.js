606f4013bb61411341dc68a639c481e35e6514ae
zoirovdev <abboszoirov17@gmail.com>
2025-04-22 17:41:10 +0500

    update

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const searchEl = document.getElementById("search");
+const containerEl = document.getElementById("container");
+
+
+
 
 
 searchEl.addEventListener("keyup", (event) => {
@@ -7,3 +11,40 @@ searchEl.addEventListener("keyup", (event) => {
 		window.location.href = `search.html?inputVal=${encodeURIComponent(inputVal)}`
 	}
 })
+
+
+getAll()
+
+function getAll(){
+	const cardEl = document.createElement("div");
+	cardEl.classList.add("card");
+
+	const anchorEl = document.createElement("a");
+	anchorEl.href = "detail.html?image=fox.jpg";
+
+	const imgEl = document.createElement("img");
+	imgEl.src = 'fox.jpg';
+	imgEl.alt = 'fox'
+
+	anchorEl.appendChild(imgEl);
+
+	const btnContainer = document.createElement("div");
+	btnContainer.classList.add("btn-container");
+
+	const btnEl = document.createElement("button");
+	btnEl.type = "button"
+	btnEl.classList.add("btn");
+	btnEl.id = "download"
+
+
+	btnContainer.appendChild(btnEl);
+
+	const iconEl = document.createElement("i");
+	iconEl.classList.add("fas", "fa-arrow-down")
+
+	btnEl.appendChild(iconEl);
+	
+	cardEl.appendChild(anchorEl);
+	cardEl.appendChild(btnContainer);
+	containerEl.appendChild(cardEl);
+}
